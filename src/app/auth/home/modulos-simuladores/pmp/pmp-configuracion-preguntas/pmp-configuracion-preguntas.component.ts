5718aef0597f28ae212d7f9577cd0c913f2a893e
Adriana Chipana <64762490+adrijonas16@users.noreply.github.com>
2023-03-14 09:23:14 -0500

    preguntas

--- src/app/auth/home/modulos-simuladores/pmp/pmp-configuracion-preguntas/pmp-configuracion-preguntas.component.ts
+++ src/app/auth/home/modulos-simuladores/pmp/pmp-configuracion-preguntas/pmp-configuracion-preguntas.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { PmpTipoRespuestaService } from 'src/app/shared/Services/Pmp-Tipo-Respuesta/pmp-tipo-respuesta.service';
 import { PmpModalAgregarPreguntasComponent } from './pmp-modal-agregar-preguntas/pmp-modal-agregar-preguntas.component';
 
 @Component({
@@ -7,27 +8,55 @@ import { PmpModalAgregarPreguntasComponent } from './pmp-modal-agregar-preguntas
   templateUrl: './pmp-configuracion-preguntas.component.html',
   styleUrls: ['./pmp-configuracion-preguntas.component.scss']
 })
-export class PmpConfiguracionPreguntasComponent implements OnInit {
+export class PmpConfiguracionPreguntasComponent implements OnInit, OnChanges {
 
   constructor(
     public dialog: MatDialog,
+    private _TipoRespuesta: PmpTipoRespuestaService
   ) { }
 
-  datasource=[]
+  datasource:any=[]
+
 
   displayedColumns: string[] = ['id', 'enunciado', 'idcategoria', 'nombrecategoria', 'nombresubcategoria'];
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.ObtenerPregunta()
   }
 
-  openDialog() {
+  ngOnInit(): void {
+    this.ObtenerPregunta()
+  }
+
+  openDialog(data:any) {
+    console.log(data)
     const dialogRef = this.dialog.open(PmpModalAgregarPreguntasComponent, {
       width: '1000px',
       maxHeight: '90vh',
       panelClass: 'dialog-gestor',
+      data: data
     });
 
     dialogRef.afterClosed().subscribe((result) => {
+    });
+  }
+
+  editar(index:number){
+
+  }
+
+  eliminar(index:number){
+
+  }
+
+  
+
+  ObtenerPregunta() {
+    this._TipoRespuesta.ObtenerPregunta().subscribe({
+      next: (x: any) => {
+        this.datasource = x;
+        console.log(x)
+      },
     });
   }
 
--- src/app/shared/Services/Pmp-Tipo-Respuesta/pmp-tipo-respuesta.service.ts
+++ src/app/shared/Services/Pmp-Tipo-Respuesta/pmp-tipo-respuesta.service.ts
@@ -1,43 +1,67 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { actualizarTipoRespuestaDTO,actualizarInterfaz } from 'src/app/Models/TipoRespuesta';
+import {
+  actualizarTipoRespuestaDTO,
+  actualizarInterfaz,
+} from 'src/app/Models/TipoRespuesta';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PmpTipoRespuestaService {
+  public urlBase = environment.url_api + 'PmpTipoRespuesta';
+  public urlBase2 = environment.url_api + 'PmpParametrosNivel';
+  public urlBase3 = environment.url_api + 'PmpTipoDominio';
+  public urlBase4 = environment.url_api + 'PmpConfiguracionSimulador';
+  public urlBase5 = environment.url_api + 'PmpPregunta';
+  public urlBase6 = environment.url_api + 'PmpTarea';
+  constructor(private http: HttpClient) {}
 
-  public urlBase=environment.url_api+'PmpTipoRespuesta';
-  public urlBase2=environment.url_api+'PmpParametrosNivel';
-  public urlBase3=environment.url_api+'PmpTipoDominio';
-  public urlBase4= environment.url_api+'PmpConfiguracionSimulador';
-  constructor(private http: HttpClient) { }
-
-
-  public ObtenerTipoRespuesta():Observable<any>{
-    return this.http.get<any>(this.urlBase+'/ObtenerTipoRespuestaEntity');
+  public ObtenerTipoRespuesta(): Observable<any> {
+    return this.http.get<any>(this.urlBase + '/ObtenerTipoRespuestaEntity');
   }
 
-  public actualizarTipoRespuesta(Json:actualizarTipoRespuestaDTO):Observable<any>{
-    console.log(Json)
-    return this.http.post<any>(this.urlBase+'/actualizarTipoRespuesta',Json);
+  public actualizarTipoRespuesta(
+    Json: actualizarTipoRespuestaDTO
+  ): Observable<any> {
+    console.log(Json);
+    return this.http.post<any>(this.urlBase + '/actualizarTipoRespuesta', Json);
   }
 
-  public ObtenerParametrosNivelEntity():Observable<any>{
-    return this.http.get<any>(this.urlBase2+'/ObtenerParametrosNivelEntity');
+  public ObtenerParametrosNivelEntity(): Observable<any> {
+    return this.http.get<any>(this.urlBase2 + '/ObtenerParametrosNivelEntity');
   }
 
-  public ObtenerDominioCategorias():Observable<any>{
-    return this.http.get<any>(this.urlBase3 +'/ObtenerDominioCategorias');
-  }
-  public ObtenerConfiguracionSimuladorEntity():Observable<any>{
-    return this.http.get<any>(this.urlBase4 +'/ObtenerConfiguracionSimuladorEntity');
+  public ObtenerDominioCategorias(): Observable<any> {
+    return this.http.get<any>(this.urlBase3 + '/ObtenerDominioCategorias');
   }
 
-  public actualizarConfiguracionSimulador(Json:actualizarInterfaz):Observable<any>{
-    console.log(Json)
-    return this.http.post<any>(this.urlBase4+'/actualizarConfiguracionSimulador',Json);
+  public ObtenerComboDominio(): Observable<any> {
+    return this.http.get<any>(this.urlBase3 + '/ObtenerComboCategorias');
+  }
+  public ObtenerConfiguracionSimuladorEntity(): Observable<any> {
+    return this.http.get<any>(
+      this.urlBase4 + '/ObtenerConfiguracionSimuladorEntity'
+    );
+  }
+
+  public actualizarConfiguracionSimulador(
+    Json: actualizarInterfaz
+  ): Observable<any> {
+    console.log(Json);
+    return this.http.post<any>(
+      this.urlBase4 + '/actualizarConfiguracionSimulador',
+      Json
+    );
+  }
+
+  public ObtenerPregunta(): Observable<any> {
+    return this.http.get<any>(this.urlBase5 + '/ObtenerListaModoDapper');
+  }
+
+  public ObtenerSubcategoriaCombo(idDominio:any): Observable<any> {
+    return this.http.post<any>(this.urlBase6 + '/ObtenerComboTarea' , idDominio[0 ]);
   }
 }
