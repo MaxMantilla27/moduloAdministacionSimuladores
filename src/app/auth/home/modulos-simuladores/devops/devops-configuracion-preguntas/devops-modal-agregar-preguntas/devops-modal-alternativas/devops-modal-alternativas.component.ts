63793937ac850560e47df734a37abfb295ef9fd9
Adriana Chipana <64762490+adrijonas16@users.noreply.github.com>
2023-03-29 10:14:03 -0500

    cambios estilo menu

--- src/app/auth/home/modulos-simuladores/devops/devops-configuracion-preguntas/devops-modal-agregar-preguntas/devops-modal-alternativas/devops-modal-alternativas.component.ts
+++ src/app/auth/home/modulos-simuladores/devops/devops-configuracion-preguntas/devops-modal-agregar-preguntas/devops-modal-alternativas/devops-modal-alternativas.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DevopsPreguntaRespuestaService } from 'src/app/shared/Services/Devops/Devops-PreguntaRespuesta/devops-pregunta-respuesta.service';
 
 @Component({
   selector: 'app-devops-modal-alternativas',
@@ -7,9 +10,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevopsModalAlternativasComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<DevopsModalAlternativasComponent>,
+    private _alternativa: DevopsPreguntaRespuestaService,
+    private formBuilder: FormBuilder,
+
+
+  ) { }
+
+  loading:any
+  loader:any;
+  listaAlternativasEnvio:any = []
+  //data:any;
+  formAlternativa: FormGroup = this.formBuilder.group({
+    Id : 0,
+    Respuesta: '',
+    Correcto : false,
+    Puntaje: 0,
+    UrlRetroalimentacionVideo: '',
+    Retroalimentacion: '',
+    UrlImagenArchivo: new File([], ''),
+  });
+
+  public selectedFilesRespuesta?: FileList;
+  public fileRespuesta: any;
+  public filestatusRespuesta = false;
+  public fileErrorMsgRespuesta = '';
+  public nombrefileRespuesta = 'Ningún archivo seleccionado';
+
 
   ngOnInit(): void {
+    console.log(this.data)
+    if(this.data!=undefined)
+    {
+      this.formAlternativa.get('Id')?.setValue(this.data[0].idAlternativa)
+      this.formAlternativa.get('Respuesta')?.setValue(this.data[0].respuesta)
+      this.formAlternativa.get('Correcto')?.setValue(this.data[0].correcto)
+      this.formAlternativa.get('Puntaje')?.setValue(this.data[0].puntaje)
+      this.formAlternativa.get('UrlRetroalimentacionVideo')?.setValue(this.data[0].urlRetroalimentacionVideo)
+      this.formAlternativa.get('Retroalimentacion')?.setValue(this.data[0].retroalimentacion)
+
+      console.log(this.formAlternativa)
+    }
+    else{
+      this.formAlternativa.reset();
+    }
   }
 
+  obtenerErrorCampoNombre(val: string) {
+    var campo = this.formAlternativa.get(val);
+    if (campo!.hasError('required')) {
+      if(val=='NombreCategoria'){
+        return 'Ingresa el nombre';
+      }
+      if(val=='Leyenda'){
+        return 'Ingresa una leyenda';
+      }
+      if(val=='contraNuevaRepeat'){
+        return 'Confirma tu nueva contraseña';
+      }
+    }
+
+    return '';
+  }
+
+  Cancelar(){
+    this.dialogRef.close();
+  }
+
+  Enviar(){
+    if(this.selectedFilesRespuesta){
+      const file: File | null = this.selectedFilesRespuesta.item(0);
+      if (file) {
+        this.formAlternativa.get('UrlImagenArchivo')?.setValue(file)
+      }
+    }
+    console.log(this.formAlternativa.value)
+
+    this.listaAlternativasEnvio=this.formAlternativa.value
+
+    console.log(this.listaAlternativasEnvio)
+    this.dialogRef.close(this.listaAlternativasEnvio);
+  }
+
+  getFileDetailsRespuesta(event: any) {
+    for (var i = 0; i < event.target.files.length; i++) {
+      this.filestatusRespuesta = true;
+      var name = event.target.files[i].name;
+      this.nombrefileRespuesta = name;
+      var type = event.target.files[i].type;
+      var size = event.target.files[i].size;
+      var modifiedDate = event.target.files[i].lastModifiedDate;
+      var extencion = name.split('.')[name.split('.').length - 1];
+      if (Math.round(size / 1024 / 1024) > 150) {
+        this.fileErrorMsgRespuesta = 'El tamanio del archivo no debe superar los 25 MB';
+        this.filestatusRespuesta = false;
+      }
+      this.selectedFilesRespuesta = event.target.files;
+      console.log(this.selectedFilesRespuesta)
+      // console.log ('Name: ' + name + "\n" +
+      //   'Type: ' + extencion + "\n" +
+      //   'Last-Modified-Date: ' + modifiedDate + "\n" +
+      //   'Size: ' + Math.round((size/1024)/1024) + " MB");
+    }
+  }
 }
